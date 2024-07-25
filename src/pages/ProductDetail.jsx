4c4eed5af22c991f67cb9e9acc3a10deadb4c3b0
Victor Koech <vickoch20@gmail.com>
2024-07-25 16:46:55 +0300

    Update import path for the useCart

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useCart } from '../context/CartContext';
+import { useCart } from '../hooks/useCart';
 import './ProductDetail.css';
 
 const ProductDetail = () => {
