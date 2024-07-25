69fe7fa9bb094ae1f08e7a525bc1a3913ca1e673
Victor Koech <vickoch20@gmail.com>
2024-07-25 16:48:24 +0300

    Update path to the useCart

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useCart } from '../context/CartContext';
+import { useCart } from '../hooks/useCart';
 import './Checkout.css';
 
 const Checkout = () => {
