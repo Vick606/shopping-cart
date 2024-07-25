a21020323a5550408c89900ab455d6d96be03a1e
Victor Koech <vickoch20@gmail.com>
2024-07-25 11:53:59 +0300

    Add ProductList to receive products as props

---
+++ src/components/shop/ProductList.jsx
@@ -0,0 +1,13 @@
+import ProductCard from './ProductCard';
+
+function ProductList({ products }) {
+  return (
+    <div className="product-list">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+}
+
+export default ProductList;
